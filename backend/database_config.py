fa7dc763a6a1c63d8d932b3b37ce9a95d1e5b98b
Alok_Sharma <aloksharma132005@gmail.com>
2026-03-22 12:31:26 +0530

    made changes in db config

--- backend/database_config.py
+++ backend/database_config.py
@@ -4,8 +4,8 @@ Supports MongoDB and PostgreSQL based on configuration.
 
 Author: Alok 
 Date: June 2025
-
 """
+
 from backend.routes.config import DB_TYPE, MONGO_URI, POSTGRES_URI
 from pymongo import MongoClient
 from sqlalchemy import create_engine
@@ -13,27 +13,35 @@ from sqlalchemy.orm import sessionmaker
 
 db = None
 SessionLocal = None
-engine = None   # <-- ADD THIS
+engine = None
 
 # MongoDB Connection 
 if DB_TYPE == "mongodb":
     try:
         mongo_client = MongoClient(MONGO_URI)
         db = mongo_client["nidra_logs"]
-        print("Database Connected")
+        print("MongoDB connected")
     except Exception as e:
-        print("Database not found",e)
+        print("MongoDB not found", e)
 
-# Postgress Connection
-elif DB_TYPE == "postgresql":
+# PostgreSQL Connection
+elif DB_TYPE in ["postgres", "postgresql"]:
     try:
         engine = create_engine(POSTGRES_URI)
-        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+        SessionLocal = sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=engine
+        )
+
+        # test connection
         conn = engine.connect()
         conn.close()
-        print("Database connected")
+
+        print("PostgreSQL connected")
+
     except Exception as e:
-        print("Database not found",e)
+        print("PostgreSQL not found", e)
 
 else:
     print(f"[Error] Unsupported DB_TYPE: {DB_TYPE}")
