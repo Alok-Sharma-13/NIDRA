38a72ebe0c3940269338c5519399ae0c1c2a0393
Alok_Sharma <aloksharma132005@gmail.com>
2026-03-22 12:39:28 +0530

    change in mongo uri

--- backend/routes/config.py
+++ backend/routes/config.py
@@ -1,6 +1,6 @@
 # F:\NIDRA\config.py
 
 DB_TYPE = "postgres"
-# MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = "mongodb://localhost:27017"
 # POSTGRES_URI = "postgresql://nidra:vvuvjljxzIiWFGbaAk3WBlCGoMfFo5u4@dpg-d6vm44vdiees73d5mmgg-a:5432/nidra"
 POSTGRES_URI="postgresql://nidra:vvuvjljxzIiWFGbaAk3WBlCGoMfFo5u4@dpg-d6vm44vdiees73d5mmgg-a.oregon-postgres.render.com:5432/nidra"
