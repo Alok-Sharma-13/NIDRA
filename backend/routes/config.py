5920125f2a19f67f18f346a20a3f6f1bee40cff0
Alok_Sharma <aloksharma132005@gmail.com>
2026-03-22 13:15:44 +0530

    change happeing

--- backend/routes/config.py
+++ backend/routes/config.py
@@ -1,6 +1,14 @@
 # F:\NIDRA\config.py
 
+# DB_TYPE = "postgres"
+# MONGO_URI = "mongodb://localhost:27017"
+# # POSTGRES_URI = "postgresql://nidra:vvuvjljxzIiWFGbaAk3WBlCGoMfFo5u4@dpg-d6vm44vdiees73d5mmgg-a:5432/nidra"
+# POSTGRES_URI="postgresql://nidra:vvuvjljxzIiWFGbaAk3WBlCGoMfFo5u4@dpg-d6vm44vdiees73d5mmgg-a.oregon-postgres.render.com:5432/nidra"
+
+
+# F:\NIDRA\config.py
+
 DB_TYPE = "postgres"
 MONGO_URI = "mongodb://localhost:27017"
-# POSTGRES_URI = "postgresql://nidra:vvuvjljxzIiWFGbaAk3WBlCGoMfFo5u4@dpg-d6vm44vdiees73d5mmgg-a:5432/nidra"
-POSTGRES_URI="postgresql://nidra:vvuvjljxzIiWFGbaAk3WBlCGoMfFo5u4@dpg-d6vm44vdiees73d5mmgg-a.oregon-postgres.render.com:5432/nidra"
+
+POSTGRES_URI = "postgresql+psycopg://nidra:vvuvjljxzIiWFGbaAk3WBlCGoMfFo5u4@dpg-d6vm44vdiees73d5mmgg-a.oregon-postgres.render.com:5432/nidra"
